400edadbf4ecd266dc202365a7621e4b88575691
José <jose@MacBook-Air-de-Usuario.local>
2022-06-18 10:25:30 +0200

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,11 +41,22 @@ const Button = styled.button`
 const ImgFeaturedProduct = styled.img`
   margin-top: 27px;
   margin-left: 82px;
+  margin-bottom: 46px;
 `;
+const ContainerTitlePhotoDay = styled.div` 
+position: absolute;
+width: 271.95px;
+height: 67px;
+left: 82px;
+top: 742px;
+background: #FFFFFF;
+text-align: center;
+`
+
 const WrapperDescriptionFeatured = styled.div`
   /* position: relative; */
   max-width: 690px;
-  margin-left: 90px;
+  /*  margin-left: 90px; */
   /* padding: 37px 68px 33px 0px; */
   display: flex;
   flex-direction: column;
@@ -67,6 +78,8 @@ const SubTitle = styled.text`
   font-weight: 700;
   font-size: 22px;
   line-height: 24px;
+  margin-top: 16px;
+  margin-bottom: 16px;
 
   color: #656565;
 `;
@@ -92,6 +105,51 @@ const TextDescriptionFooter = styled.text`
   margin-top: 40px;
 `;
 
+const ContainerFlex = styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: 1280px;
+  margin-left: 92px;
+  /* background: red; */
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* justify-content: space-between; */
+`;
+const Row = styled.div`
+  display: flex;
+  /* justify-content: end; */
+  /*  background: red; */
+  margin-top: 34px;
+  margin-bottom: 54px;
+`;
+const Img = styled.img`
+  width: 117px;
+  height: 147px;
+  margin-left: 35px;
+`;
+const DimensionsDetails = styled.text`
+  font-family: "Archivo";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 17px;
+  color: #656565;
+  text-align: right;
+  margin-top: 16px;
+  margin-bottom: 17px;
+`;
+const SizeDetails = styled.text`
+  font-family: "Archivo";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 17px;
+  color: #656565;
+  text-align: right;
+`;
 
 const Header = () => {
   return (
@@ -101,23 +159,44 @@ const Header = () => {
         <Button>add to cart</Button>
       </Wrapper>
       <ImgFeaturedProduct src="/images/featured-product.png" />
+      <ContainerTitlePhotoDay>
+        <Title sytle={{fontSize: "20px"}}>Photo of the day</Title>
+      </ContainerTitlePhotoDay>
 
-      <WrapperDescriptionFeatured>
-        <Title>About the Samurai King Resting</Title>
-        <SubTitle>Pets</SubTitle>
-        <TextDescription>
-          So how did the classical Latin become so incoherent? According to
-          McClintock, a 15th century typesetter likely scrambled part of
-          Cicero's De Finibus in order to provide placeholder text to mockup
-          various fonts for a type specimen book.So how did the classical Latin
-          become so incoherent? According to McClintock, a 15th century
-          typesetter likely scrambled part of Cicero's De Finibus in order to
-          provide placeholder 
-        </TextDescription>
-        <TextDescriptionFooter>text to mockup various fonts for a type specimen book.So how did the
-          classical Latin become so incoherent? According to McClintock.</TextDescriptionFooter>
-        
-      </WrapperDescriptionFeatured>
+      <ContainerFlex>
+        <WrapperDescriptionFeatured>
+          <Title>About the Samurai King Resting</Title>
+          <SubTitle>Pets</SubTitle>
+          <TextDescription>
+            So how did the classical Latin become so incoherent? According to
+            McClintock, a 15th century typesetter likely scrambled part of
+            Cicero's De Finibus in order to provide placeholder text to mockup
+            various fonts for a type specimen book.So how did the classical
+            Latin become so incoherent? According to McClintock, a 15th century
+            typesetter likely scrambled part of Cicero's De Finibus in order to
+            provide placeholder
+          </TextDescription>
+          <TextDescriptionFooter>
+            text to mockup various fonts for a type specimen book.So how did the
+            classical Latin become so incoherent? According to McClintock.
+          </TextDescriptionFooter>
+        </WrapperDescriptionFeatured>
+
+        <Column>
+          <Title style={{ textAlign: "right"}}>People also buy</Title>
+
+          <Row>
+            <Img src="/images/yellow-wall.png" />
+            <Img src="/images/odette-window.png" />
+            <Img src="/images/egg-ballon.png" />
+          </Row>
+          <Column>
+            <Title style={{ textAlign: "right"}}>Details</Title>
+            <DimensionsDetails>Size: 1020 x 1020 pixel</DimensionsDetails>
+            <SizeDetails>Size: 15 mb</SizeDetails>
+          </Column>
+        </Column>
+      </ContainerFlex>
     </>
   );
 };
